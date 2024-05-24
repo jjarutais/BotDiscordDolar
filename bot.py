208b2f22fa9a59013634a5951cc10b077a01284d
jjarutais <joao.jarutais1@gmail.com>
2024-05-24 04:19:21 -0300

    final commit
    final commit

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ async def send_dolar_rate():
             data = response.json()
             dolar_rate = float(data['USDBRL']['bid'])
             await channel.send(f'>>@task.loop<<')
-            await channel.send(f'O valor do dolar em reais às AJUSTAR O HORÁRIO do dia AJUSTAR O DIA é R${dolar_rate:.4f}')
+            await channel.send(f'O valor do dolar em reais agora é R${dolar_rate:.2f}')
         else:
             print('Canal não encontrado ou sem permissão para enviar mensagens.')
     except requests.exceptions.RequestException as e:
@@ -41,17 +41,42 @@ async def dolar(ctx):
         response.raise_for_status()  # Levanta um erro para status de resposta HTTP ruins
         data = response.json()
         dolar_rate = float(data['USDBRL']['bid'])
-        await ctx.send(f'O valor do dólar em reais agora é R${dolar_rate:.4f}')
+        await ctx.send(f'O valor do dólar em reais agora é R${dolar_rate:.2f}')
     except requests.exceptions.RequestException as e:
         print(f'Erro ao buscar a taxa de câmbio: {e}')
         await ctx.send('Ocorreu um erro ao buscar a taxa de câmbio.')
 
+@bot.command(name='euro')
+async def euro(ctx):
+    try:
+        response = requests.get('https://economia.awesomeapi.com.br/last/EUR-BRL')
+        response.raise_for_status()  # Levanta um erro para status de resposta HTTP ruins
+        data = response.json()
+        euro_rate = float(data['EURBRL']['bid'])
+        await ctx.send(f'O valor do euro em reais agora é R${euro_rate:.2f}')
+    except requests.exceptions.RequestException as e:
+        print(f'Erro ao buscar a taxa de câmbio: {e}')
+        await ctx.send('Ocorreu um erro ao buscar a taxa de câmbio.')
+
+@bot.command(name='bitcoin')
+async def bitcoin(ctx):
+    try:
+        response = requests.get('https://economia.awesomeapi.com.br/last/BTC-BRL')
+        response.raise_for_status()  # Levanta um erro para status de resposta HTTP ruins
+        data = response.json()
+        bitcoin_rate = float(data['BTCBRL']['bid'])
+        await ctx.send(f'O valor do euro em reais agora é R${bitcoin_rate:.2f}')
+    except requests.exceptions.RequestException as e:
+        print(f'Erro ao buscar a taxa de câmbio: {e}')
+        await ctx.send('Ocorreu um erro ao buscar a taxa de câmbio.')
 
 @bot.command(name='ajuda')
 async def commands_list(ctx):
     embed = discord.Embed(title="Lista de Comandos", description="Lista de comandos disponíveis para uso", color=discord.Color.blue())
     embed.add_field(name="!dolar", value="Obtenha a taxa de câmbio atual do dólar em reais.", inline=False)
-    embed.add_field(name="!conversor", value="Converta uma quantia de uma moeda para outra.", inline=False)
+    embed.add_field(name="!euro", value="Obtenha a taxa de câmbio atual do euro em reais.", inline=False)
+    embed.add_field(name="!bitcoin", value="Obtenha a taxa de câmbio atual do bitcoin em reais.", inline=False)
+    embed.add_field(name="!conversor", value="Converta uma quantia de uma moeda para outra. Exemplo: !conversor 100 USD BRL", inline=False)
     await ctx.send(embed=embed)
 
 @bot.event
@@ -59,17 +84,27 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("Este comando não existe. Use `!ajuda` para ver a lista de comandos disponíveis.")
 
+
 @bot.command(name='conversor')
-async def conversor(ctx, quantidade: float, moeda_origem: str, moeda_destino: str):
+async def conversor(ctx, quantidade: str, moeda_origem: str, moeda_destino: str):
+    try:
+        quantidade = float(quantidade.replace(',', '.'))
+    except ValueError:
+        await ctx.send("Por favor, insira um valor numérico válido para a quantidade.")
+        return
+
     url = f"https://economia.awesomeapi.com.br/last/{moeda_origem}-{moeda_destino}"
 
     try:
         response = requests.get(url)
+        response.raise_for_status()  # Levanta um erro para status de resposta HTTP ruins
         data = response.json()
         taxa_conversao = float(data[f'{moeda_origem}{moeda_destino}']['bid'])
         resultado = quantidade * taxa_conversao
 
         await ctx.send(f"{quantidade} {moeda_origem} equivale a {resultado:.2f} {moeda_destino}")
+    except KeyError:
+        await ctx.send("Ocorreu um erro ao processar a conversão: Moeda não encontrada ou formato incorreto.")
     except Exception as e:
         await ctx.send(f"Ocorreu um erro ao processar a conversão: {e}")
 
